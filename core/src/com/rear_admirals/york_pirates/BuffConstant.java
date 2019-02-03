14fb4654bc2eba3cc2ae0f26a378379736cadede
Jiahaowangcharles <jw2616@york.ac.uk>
2019-02-03 16:50:23 +0000

    buff Constant class

---
+++ core/src/com/rear_admirals/york_pirates/BuffConstant.java
@@ -0,0 +1,15 @@
+package com.rear_admirals.york_pirates;
+
+public class BuffConstant {
+    public String ATTACK_BUFF_TAG = "attack";
+    public int ATTACK_BUFF = 10;
+    public int ATTACK_BUFF_PRICE = 10;
+    public int ARRACK_BUFF_TURNS = 2;
+
+    public String ACCURACY_BUFF_TAG = "accuracy";
+    public int ACCURACY_BUFF = 3;
+    public int ACCURACY_BUFF_PRICE = 20;
+    public int ACCURACY_BUFF_TURN = 2;
+
+
+}
